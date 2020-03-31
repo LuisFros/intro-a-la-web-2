10f636e45839612765af61a37f29f4e5b6d459dd
LuisFros <lmfros@uc.cl>
2020-03-31 14:26:56 -0300

    feat(): solucion

--- practico/script.js
+++ practico/script.js
@@ -3,29 +3,88 @@ const contenedor = document.getElementById('lists-container');
 
 const crearBotonMarcar = (nodoTarea) => {
   const nuevoBoton = document.createElement('button');
+  nuevoBoton.innerText = "Marcar"
+  nuevoBoton.addEventListener('click',()=>{
+    if(nodoTarea.classList.contains('terminada')){
+      nodoTarea.classList.remove('terminada')
+    } else{
+      nodoTarea.classList.add('terminada')
+    }
+  })
   // rellenar aquí
   return nuevoBoton;
 };
 
 const crearBotonRemover = (nodoLista, nodoTarea) => {
   const nuevoBoton = document.createElement('button');
+  nuevoBoton.innerText = "Remover"
+  nuevoBoton.addEventListener('click',()=>{
+    nodoLista.removeChild(nodoTarea)
+  })
   // rellenar aquí
   return nuevoBoton;
 };
 
 const agregarNodoTarea = (nodoLista, tarea, nodoInput) => {
-  // rellenar aquí
+  
+  const nodoContenedor = document.createElement('div');
+  const { texto }= tarea;
+
+  const nodoTarea = document.createElement('a')
+  nodoTarea.innerText = `-${texto}`
+  nodoTarea.className = 'tarea'
+  nodoContenedor.appendChild(nodoTarea)
+  const nodoBotonMarcar = crearBotonMarcar(nodoTarea)
+  const nodoBotonRemover = crearBotonRemover(nodoLista, nodoContenedor)
+  nodoContenedor.appendChild(nodoBotonMarcar)
+  nodoContenedor.appendChild(nodoBotonRemover)
+  nodoLista.insertBefore(nodoContenedor,nodoInput)
 };
 
 const crearInputDeTareas = (nodoLista) => {
   const nodoContenedor = document.createElement('div');
+  const nodoTexto = document.createTextNode('Nueva Tarea');
+  const nodoInput = document.createElement('input')
+
   // rellenar aquí
+  nodoContenedor.appendChild(nodoTexto)
+  nodoContenedor.appendChild(nodoInput)
+  // Agregamos un evento de tecla oprimida o 'keydown'
+  nodoInput.addEventListener('keydown', (evento) => {
+    const codigo_tecla = evento.keyCode;
+    const valor_texto = evento.target.value;
+    // codigo_tecla contiene un código de la tecla apretada
+    // valor_texto contiene el valor dentro del input al oprimir la tecla
+    if (codigo_tecla === 13) { // el código 13 es para la tecla enter
+      
+      const nuevaTarea = {texto:valor_texto, terminado:false};
+
+      agregarNodoTarea(nodoLista,nuevaTarea,nodoContenedor);
+    }
+});
+
   return nodoContenedor;
 };
 
 const agregarNodoLista = (lista) => { 
   const nodoLista = document.createElement('div');
+  nodoLista.className = 'lista'
   // rellenar aquí
+  const {titulo } = lista;
+  const nodoTitulo = document.createTextNode(titulo)
+  nodoLista.appendChild(nodoTitulo)
+
+  const nodoInputTareas = crearInputDeTareas(nodoLista)
+  nodoLista.appendChild(nodoInputTareas)
+  
+  if(lista.tareas.length > 0){
+    const {tareas} = lista
+    tareas.forEach(tarea => {
+      agregarNodoTarea(nodoLista,tarea,nodoInputTareas)
+    });
+  }
+  
+  
   contenedor.appendChild(nodoLista);
 }
 
@@ -37,8 +96,8 @@ inputListas.addEventListener('keydown', (evento) => {
   const valor_texto = evento.target.value;
   // codigo_tecla contiene un código de la tecla apretada
   // valor_texto contiene el valor dentro del input al oprimir la tecla
-
   if (codigo_tecla === 13) { // el código 13 es para la tecla enter
+    
     const nuevaLista = {titulo: valor_texto, tareas: []};
     agregarNodoLista(nuevaLista);
   }
